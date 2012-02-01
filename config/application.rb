0947102db56a635e084f8c330f1432254b6fc42e
Sammy Larbi <sam@codeodor.com>
2012-01-31 18:15:22 -0600

    install devise for user auth

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module RailsOopPresentation
   class Application < Rails::Application
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RailsOopPresentation::Application.routes.draw do
+  root :to => "home#index"
+  
+  devise_for :users
+
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
