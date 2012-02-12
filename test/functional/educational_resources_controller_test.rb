1ed849dee65f6675694c93a74fe158b90bd1834c
Sammy Larbi <sam@codeodor.com>
2012-02-12 09:17:55 -0600

    #1 fix educationl resources controller tests

--- test/functional/educational_resources_controller_test.rb
+++ test/functional/educational_resources_controller_test.rb
@@ -7,44 +7,44 @@ class EducationalResourcesControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, resource_list_id: @educational_resource.resource_list_id
     assert_response :success
     assert_not_nil assigns(:educational_resources)
   end
 
   test "should get new" do
-    get :new
+    get :new, resource_list_id: @educational_resource.resource_list_id
     assert_response :success
   end
 
   test "should create educational_resource" do
     assert_difference('EducationalResource.count') do
-      post :create, educational_resource: @educational_resource.attributes
+      post :create, educational_resource: @educational_resource.attributes, resource_list_id: @educational_resource.resource_list_id
     end
 
-    assert_redirected_to educational_resource_path(assigns(:educational_resource))
+    assert_redirected_to resource_list_educational_resource_path(assigns(:educational_resource).resource_list, assigns(:educational_resource))
   end
 
   test "should show educational_resource" do
-    get :show, id: @educational_resource
+    get :show, id: @educational_resource, resource_list_id: @educational_resource.resource_list_id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @educational_resource
+    get :edit, id: @educational_resource, resource_list_id: @educational_resource.resource_list_id
     assert_response :success
   end
 
   test "should update educational_resource" do
-    put :update, id: @educational_resource, educational_resource: @educational_resource.attributes
-    assert_redirected_to educational_resource_path(assigns(:educational_resource))
+    put :update, id: @educational_resource, educational_resource: @educational_resource.attributes, resource_list_id: @educational_resource.resource_list_id
+    assert_redirected_to resource_list_educational_resource_path(assigns(:educational_resource).resource_list, assigns(:educational_resource))
   end
 
   test "should destroy educational_resource" do
     assert_difference('EducationalResource.count', -1) do
-      delete :destroy, id: @educational_resource
+      delete :destroy, id: @educational_resource, resource_list_id: @educational_resource.resource_list_id
     end
 
-    assert_redirected_to educational_resources_path
+    assert_redirected_to resource_list_educational_resources_path(@educational_resource.resource_list)
   end
 end
