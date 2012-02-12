985b7c9211513dea57de00b554d1d8c5bfb5c054
Sammy Larbi <sam@codeodor.com>
2012-02-12 09:44:03 -0600

    #1 fix sections tests

--- test/functional/sections_controller_test.rb
+++ test/functional/sections_controller_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class SectionsControllerTest < ActionController::TestCase
   setup do
     sign_in users(:admin)
-    @section = sections(:one)
+    @section = sections(:MWF)
   end
 
   test "should get index" do
-    get :index
+    get :index, department_id: @section.course.department.id, course_id: @section.course_id
     assert_response :success
     assert_not_nil assigns(:sections)
   end
 
   test "should get new" do
-    get :new
+    get :new, department_id: @section.course.department.id, course_id: @section.course_id
     assert_response :success
   end
 
@@ -22,29 +22,29 @@ class SectionsControllerTest < ActionController::TestCase
       post :create, section: @section.attributes
     end
 
-    assert_redirected_to section_path(assigns(:section))
+    assert_redirected_to department_course_section_path(assigns(:section).course.department, assigns(:section).course, assigns(:section))
   end
 
   test "should show section" do
-    get :show, id: @section
+    get :show, id: @section, department_id: @section.course.department.id, course_id: @section.course_id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @section
+    get :edit, id: @section, department_id: @section.course.department.id, course_id: @section.course_id
     assert_response :success
   end
 
   test "should update section" do
     put :update, id: @section, section: @section.attributes
-    assert_redirected_to section_path(assigns(:section))
+    assert_redirected_to department_course_section_path(assigns(:section).course.department, assigns(:section).course, assigns(:section))
   end
 
   test "should destroy section" do
     assert_difference('Section.count', -1) do
-      delete :destroy, id: @section
+      delete :destroy, id: @section, department_id: @section.course.department.id, course_id: @section.course_id
     end
 
-    assert_redirected_to sections_path
+    assert_redirected_to department_course_sections_path(@section.course.department, @section.course)
   end
 end
